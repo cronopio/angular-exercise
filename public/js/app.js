5bc1ad419e59685067f0613f9b911051c029d0da
cronopio <aristizabal.daniel@gmail.com>
2015-02-12 01:33:32 -0500

    Inicio la dibujada del mapa

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,21 @@ function mainController ($scope, $http) {
     $http.post('/login', $scope.user)
       .success(function (data) {
         $scope.user.auth = data;
+        dibujarMapa();
       })
       .error(function (data) {
         console.log('Error', data);
       });
   }
+}
+
+function dibujarMapa () {
+  $('body').empty();
+  $('html, body').css('height', '100%').css('width', '100%');
+  $('body').append($('<div id=\"map-canvas\"></div>').css('height', '100%').css('width', '100%').css('margin', '0px').css('padding', '0px'));
+  var a = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 13,
+    center: new google.maps.LatLng(37.774546, -122.433523),
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  });
 }
