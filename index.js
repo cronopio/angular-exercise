9eb8a03005fa49fafb98c7ddc7c5c02f3f1f6fde
cronopio <aristizabal.daniel@gmail.com>
2015-02-11 23:55:16 -0500

    La url del endpoint de la API a utilizar de backend se especifica usando --api

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var colors = require('colors'),
+    argv = require('optimist').string('api').argv;
     server = require('./lib/server.js');
 
 var PORT = 3000;
+
+if (argv.h || argv.help || !argv.api) {
+  console.log([
+    "usage: node index.js --api <backendAPI>",
+    "",
+    "options:",
+    "  --api        URL of the backend api",
+    "",
+    "  -h --help    Print this list and exit."
+  ].join('\n'));
+  process.exit();
+}
 
 server.createServer({
   headers: {
     'x-powered-by': 'Backend for the Exercise v0.0.1'
   },
-  api: 'http://backend.api/'
+  api: argv.api
 }).listen(PORT, function () {
   console.log('Server started on port:'.yellow, PORT.toString().cyan);
+  console.log('Backend API:'.yellow, argv.api.cyan);
   console.log('Hit CTRL-C to stop the server'.gray);
 });
 
